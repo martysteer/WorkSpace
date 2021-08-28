dbfa0a36991dca11b3365d50dbf3d695f95892c0
Marty <martysteer@gmail.com>
2021-08-28 15:45:03 +0100

    refactoring into javascript module

--- static/js/hologram/Scene.js
+++ static/js/hologram/Scene.js
@@ -1,144 +1,10 @@
 // ----------------------------------------------------------
-// Custom shaders
-// ----------------------------------------------------------
-THREE.VolumetericLightShader = {
-  uniforms: {
-    tDiffuse: {
-      value: null
-    },
-    lightPosition: {
-      value: new THREE.Vector2(0.5, 0.5)
-    },
-    exposure: {
-      value: 1
-    },
-    decay: {
-      value: 1
-    },
-    density: {
-      value: 6
-    },
-    weight: {
-      value: 0.57
-    },
-    samples: {
-      value: 30
-    }
-  },
-
-
-  vertexShader: [
-    "varying vec2 vUv;",
-    "void main() {",
-    "vUv = uv;",
-    "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
-    "}"
-  ].
-  join("\n"),
-
-  fragmentShader: [
-    "varying vec2 vUv;",
-    "uniform sampler2D tDiffuse;",
-    "uniform vec2 lightPosition;",
-    "uniform float exposure;",
-    "uniform float decay;",
-    "uniform float density;",
-    "uniform float weight;",
-    "uniform int samples;",
-    "const int MAX_SAMPLES = 100;",
-    "void main()",
-    "{",
-    "vec2 texCoord = vUv;",
-    "vec2 deltaTextCoord = texCoord - lightPosition;",
-    "deltaTextCoord *= 1.0 / float(samples) * density;",
-    "vec4 color = texture2D(tDiffuse, texCoord);",
-    "float illuminationDecay = 1.0;",
-    "for(int i=0; i < MAX_SAMPLES; i++)",
-    "{",
-    "if(i == samples) {",
-    "break;",
-    "}",
-    "texCoord += deltaTextCoord;",
-    "vec4 sample = texture2D(tDiffuse, texCoord);",
-    "sample *= illuminationDecay * weight;",
-    "color += sample;",
-    "illuminationDecay *= decay;",
-    "}",
-    "gl_FragColor = color * exposure;",
-    "}"
-  ].
-  join("\n")
-};
-
-THREE.AdditiveBlendingShader = {
-  uniforms: {
-    tDiffuse: {
-      value: null
-    },
-    tAdd: {
-      value: null
-    }
-  },
-
-
-  vertexShader: [
-    "varying vec2 vUv;",
-    "void main() {",
-    "vUv = uv;",
-    "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
-    "}"
-  ].
-  join("\n"),
-
-  fragmentShader: [
-    "uniform sampler2D tDiffuse;",
-    "uniform sampler2D tAdd;",
-    "varying vec2 vUv;",
-    "void main() {",
-    "vec4 color = texture2D(tDiffuse, vUv);",
-    "vec4 add = texture2D(tAdd, vUv);",
-    "gl_FragColor = color + add;",
-    "}"
-  ].
-  join("\n")
-};
-
-THREE.PassThroughShader = {
-  uniforms: {
-    tDiffuse: {
-      value: null
-    }
-  },
-
-
-  vertexShader: [
-    "varying vec2 vUv;",
-    "void main() {",
-    "vUv = uv;",
-    "gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);",
-    "}"
-  ].
-  join("\n"),
-
-  fragmentShader: [
-    "uniform sampler2D tDiffuse;",
-    "varying vec2 vUv;",
-    "void main() {",
-    "gl_FragColor = texture2D(tDiffuse, vec2(vUv.x, vUv.y));",
-    "}"
-  ].
-  join("\n")
-};
-
-
-
-// ----------------------------------------------------------
 // Scene.js
 // ----------------------------------------------------------
 const DEFAULT_LAYER = 0;
 const OCCLUSION_LAYER = 1;
 const lightColor = 0x0099ff;
-const renderScale = .25;
+const renderScale = 0.02;
 const clock = new THREE.Clock();
 
 
@@ -229,7 +95,21 @@ class HoloScene {
     this.cube = new THREE.Mesh(geometry, material);
     this.cube.position.x = -2;
     this.cube.position.y = 3;
-    this.scene.add(this.cube);
+    // this.scene.add(this.cube);
+
+
+    // Hologram's image as texture on a flat mesh
+    var planeGeo = new THREE.PlaneGeometry(this.img.offsetWidth * renderScale, this.img.offsetHeight * renderScale);
+    const loader = new THREE.TextureLoader();
+    loader.load(this.img.src, (texture) => {
+      const imgMaterial = new THREE.MeshBasicMaterial({
+        map: texture,
+        transparent: true,
+        opacity: 0.7
+      });
+      this.imgPlane = new THREE.Mesh(planeGeo, imgMaterial);
+      this.scene.add(this.imgPlane);
+    });
 
 
     // The geometry of the holographic image is a simple plane
@@ -268,7 +148,7 @@ class HoloScene {
       itemMaterial.map = itemTexture;
 
       parent.itemMesh = new THREE.Mesh(itemGeo, itemMaterial); // class field
-      parent.scene.add(parent.itemMesh);
+      // parent.scene.add(parent.itemMesh);
 
       // Create occlusion materials/mesh and add to parent.scene
       // (This is the holographic illumination layer on top)
@@ -315,6 +195,10 @@ class HoloScene {
     if (this.itemMesh) {
       this.itemMesh.rotation.y = Math.sin(elapsed / 2) / 15;
       this.itemMesh.rotation.z = Math.cos(elapsed / 2) / 50;
+
+      this.imgPlane.rotation.y = Math.sin(elapsed / 2) / 15;
+      this.imgPlane.rotation.z = Math.cos(elapsed / 2) / 50;
+
 
       // this.occMesh.rotation.copy(this.itemMesh.rotation);
     }
--- static/js/hologram/hologram.js
+++ static/js/hologram/hologram.js
@@ -1,114 +1,26 @@
-
-const theGUI = new dat.GUI();
-setupGUI(theGUI);
-
-/* Set up the parameter configuration GUI (from codepen.io) */
-function setupGUI(aGUI) {
-  let folder,
-    min,
-    max,
-    step,
-    updateShaderLight = function() {
-      const p = lightSource.position.clone(),
-        vector = p.project(camera),
-        x = (vector.x + 1) / 2,
-        y = (vector.y + 1) / 2;
-      vlShaderUniforms.lightPosition.value.set(x, y);
-    };
-
-  updateShaderLight();
-
-  // Bloom Controls
-  folder = aGUI.addFolder('Bloom');
-  folder.add(bloomPass, 'radius').
-  min(0).
-  max(10).
-  name('Radius');
-  folder.add(bloomPass, 'threshold').
-  min(0).
-  max(1).
-  name('Threshold');
-  folder.add(bloomPass, 'strength').
-  min(0).
-  max(10).
-  name('Strength');
-  folder.open();
-
-  // Bad TV Controls
-  folder = aGUI.addFolder('TV');
-  folder.add(badTVPass.uniforms.distortion, 'value').
-  min(0).
-  max(10).
-  name('Distortion 1');
-  folder.add(badTVPass.uniforms.distortion2, 'value').
-  min(0).
-  max(10).
-  name('Distortion 2');
-  folder.add(badTVPass.uniforms.speed, 'value').
-  min(0).
-  max(1).
-  name('Speed');
-  folder.add(badTVPass.uniforms.rollSpeed, 'value').
-  min(0).
-  max(10).
-  name('Roll Speed');
-  folder.open();
-
-  // Light Controls
-  folder = aGUI.addFolder('Light Position');
-  folder.add(lightSource.position, 'x').
-  min(-50).
-  max(50).
-  onChange(updateShaderLight);
-  folder.add(lightSource.position, 'y').
-  min(-50).
-  max(50).
-  onChange(updateShaderLight);
-  folder.add(lightSource.position, 'z').
-  min(-50).
-  max(50).
-  onChange(updateShaderLight);
-  folder.open();
-
-  // Volumetric Light Controls
-  folder = aGUI.addFolder('Volumeteric Light Shader');
-  folder.add(vlShaderUniforms.exposure, 'value').
-  min(0).
-  max(1).
-  name('Exposure');
-  folder.add(vlShaderUniforms.decay, 'value').
-  min(0).
-  max(1).
-  name('Decay');
-  folder.add(vlShaderUniforms.density, 'value').
-  min(0).
-  max(10).
-  name('Density');
-  folder.add(vlShaderUniforms.weight, 'value').
-  min(0).
-  max(1).
-  name('Weight');
-  folder.add(vlShaderUniforms.samples, 'value').
-  min(1).
-  max(100).
-  name('Samples');
-
-  folder.open();
-}
+/**
+ * Hologram Scene class for Holographic Projection of an image into a conatiner element
+ * 
+ * A Pen created on CodePen.io.
+ * Original URL: [https://codepen.io/peterhry/pen/egzjGR](https://codepen.io/peterhry/pen/egzjGR).
+ * 
+ * Modified, commented and adapted by Marty Steer, MythAxis Magazine, 2021
+ **/
 
 
-function addRenderTargetImage() {
-  const material = new THREE.ShaderMaterial(THREE.PassThroughShader);
-  material.uniforms.tDiffuse.value = occRenderTarget.texture;
+// ----------------------------------------------------------
+// Hologram.js
+// ----------------------------------------------------------
 
-  const mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2, 2), material);
-  composer.passes[1].scene.add(mesh);
-  mesh.visible = false;
+class Hologram {
+  // Pass a container element to use for the hologram.
+  constructor(container) {}
 
-  const folder = gui.addFolder('Light Pass Render Image');
-  folder.add(mesh, 'visible');
-  folder.open();
-}
+  // Render the scene
+  render() {}
 
 
-addRenderTargetImage();
+
+} // end class HoloScene
+
+export { Hologram };
